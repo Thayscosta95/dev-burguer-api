01b6d912abe1ae096f47052990763965611e3902
Pichau <Thayscosta95>
2026-04-22 10:06:56 -0300

    feat(database): adiciona configuração do Sequelize e modelo User feat(routes): cria rotas para gerenciamento de usuários feat(controllers): implementa UserController com método de criação de usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -13,12 +13,38 @@ Se precisar criar outro método, tem que ser com outro nome.
 import { response } from 'express';
 import User from '../models/User.js';
 import { v4 } from 'uuid';
+import * as Yup from 'yup';
 
 class UserController {
 
     async store( request, response) {
+
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().min(6).required(),
+            admin: Yup.boolean().required()
+        })
+
+        try{
+            schema.validateSync(request.body, { abortEarly: false, strict: true });
+        }catch(err){
+           
+            return response.status(400).json({error: err.errors});
+        }
+      
        
         const {name, email, password_hash, admin}= request.body;
+
+        const existingUser = await User.findOne({
+            where: { 
+                email }
+        })
+        if(existingUser){
+            return response
+            .status(400)
+            .json({message: 'Email already taken!'})
+        }
         
     const user = await User.create({
         id: v4(),
