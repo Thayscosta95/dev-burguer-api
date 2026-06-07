dbfb1b94713f7fed9760f6f9da16624287379a0a
Pichau <tha_tacosta@hotmail.com>
2026-06-07 14:53:25 -0300

    feat: adiciona suporte ao CORS e configurações de workspace

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import routes from './routes.js';
 import fileRouteConfig from './config/fileRoutes.cjs';
+import  cors from 'cors';
 
 const app = express();
 
@@ -8,7 +9,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/product-file', fileRouteConfig);
 app.use('/category-file', fileRouteConfig);
+app.use(cors());
 
 app.use(routes);
+
+
 
 export default app;
